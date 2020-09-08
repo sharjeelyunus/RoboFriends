05597569535fb520ea5fa6f1bf684cc405e9c28d
sharjeelyunus <sharjeel924@gmail.com>
2020-09-08 18:40:50 +0500

    👌 IMPROVE: Card Array ➕

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+    <CardList robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
