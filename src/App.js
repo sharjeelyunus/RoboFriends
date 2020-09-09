4bcdd824c30ad5de5b0c3fd699dad5e69d38af73
sharjeelyunus <sharjeel924@gmail.com>
2020-09-10 01:13:05 +0500

    👌 IMPROVE: Added Functionaliy to Search Box 🛠

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange}) => {
     return (
         <div className='pa2'>
             <input
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search'
                 placeholder='Search Robots'
+                onChange={searchChange}
             />
         </div>
     );
