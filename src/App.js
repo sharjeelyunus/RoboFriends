4497321fd3923b6092b9e9a09d2afde622670f9a
sharjeelyunus <sharjeel924@gmail.com>
2020-09-12 02:50:56 +0500

    👌 IMPROVE: Scroll CardList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -34,7 +35,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={filteredRobots} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
                 </div>
             );
         }
