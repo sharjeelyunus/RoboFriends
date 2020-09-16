546b33d3eab2e34cc04693e24dca9ebb06b8d9a0
sharjeelyunus <sharjeel924@gmail.com>
2020-09-17 00:34:04 +0500

    👌 IMPROVE: Added Store & Provider (Redux)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,7 @@
 import { CHANGE_SEARCH_FIELD} from './constants';
 
 const initialState = {
-    searchField = ''
+    searchField: ''
 }
 
 export const searchRobots = (state=initialState, action={}) => {
