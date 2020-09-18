079045a8ea00186d98283a4eae081af72d42792d
sharjeelyunus <sharjeel924@gmail.com>
2020-09-19 00:11:44 +0500

    📦 NEW: Redux Middleware ➕

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { searchRobots } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.render(
   <React.StrictMode>
