2d2a0d0b22dac199f074187c3260b2319768220f
sharjeelyunus <sharjeel924@gmail.com>
2020-09-20 03:01:53 +0500

    🐛 FIX: Errors

--- src/reducers.js
+++ src/reducers.js
@@ -1,38 +1,37 @@
-import { 
-    CHANGE_SEARCH_FIELD,
-    REQUEST_ROBOTS_PENDING,
-    REQUEST_ROBOTS_SUCCESS,
-    REQUEST_ROBOTS_FAILED,
-} from './constants';
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+ } from './constants';
 
 const initialStateSearch = {
-    searchField: ''
+  searchField: ''
 }
 
 export const searchRobots = (state=initialStateSearch, action={}) => {
-    switch(action.type) {
-        case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchRobots: action.payload});
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CHANGE_SEARCHFIELD:
+      return Object.assign({}, state, {searchField: action.payload})
+    default:
+      return state
+  }
 }
 
 const initialStateRobots = {
-    isPending: false,
-    robots: [],
-    error: ''
+  robots: [],
+  isPending: true
 }
 
 export const requestRobots = (state=initialStateRobots, action={}) => {
-    switch (action.type) {
-        case REQUEST_ROBOTS_PENDING:
-            return Object.assign({}, state, { isPending: true})
-        case REQUEST_ROBOTS_SUCCESS:
-            return Object.assign({}, state, { robots: action.payload, isPending: false})
-        case REQUEST_ROBOTS_FAILED:
-            return Object.assign({}, state, { error: action.payload, isPending: false})
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return Object.assign({}, state, {isPending: true})
+    case REQUEST_ROBOTS_SUCCESS:
+      return Object.assign({}, state, {robots: action.payload, isPending: false})
+    case REQUEST_ROBOTS_FAILED:
+      return Object.assign({}, state, {error: action.payload})
+    default:
+      return state
+  }
 }
